e23da6c5381e0b092cc91e63e9f2aa3f00417a8e
iconie(聂小涛) <iconie@tencent.com>
2017-08-03 15:30:18 +0800

    update main function:improve writting rules

--- src/FrameAnimationCanvas.js
+++ src/FrameAnimationCanvas.js
@@ -2,9 +2,8 @@
  * Created by iconie on 2017/7/13.
  *
  */
-class frameAnimation{
-
-    constructor(imgSrc,canvasDom,positionInfo,interFrameTime){
+class frameAnimation {
+    constructor(imgSrc, canvasDom, positionInfo, interFrameTime) {
         this.imgSrc = imgSrc;
         this.canvasDom = canvasDom;
         this.positionInfo = positionInfo;
@@ -14,7 +13,7 @@ class frameAnimation{
         this.ifstop = false;
         this.schedule = 0;
         this.interFrameTime = interFrameTime;
-        this.imgOnload=false;
+        this.imgOnload = false;
 
         this.begin = this.begin.bind(this);
         this.stop = this.stop.bind(this);
@@ -22,96 +21,97 @@ class frameAnimation{
         this.end = this.end.bind(this);
     }
 
-    drawCertain(index){
-        //相当于先要end
+    drawCertain(index) {
+        // 相当于先要end
         this.ifstop = false;
         this.ifbegin = false;
         this.frameIndex = 0;
         clearTimeout(this.timeOutIndex);
 
-        let that = this,
-            ctx = this.canvasDom.getContext('2d'),
-            img = new Image();
+        let that = this;
+        let ctx = this.canvasDom.getContext('2d');
+        let img = new Image();
 
-        img.onload = function(){
+        img.onload = function() {
+            // 可能之前会有占位静态图的情况
+            that.canvasDom.style.backgroundImage = 'none';
 
-            //可能之前会有占位静态图的情况
-            that.canvasDom.style.backgroundImage = "";
-
-            let beginX,beginY,realX,realY;
+            let beginX;
+            let beginY;
+            let realX;
+            let realY;
 
             realX = that.positionInfo[index].w * (that.canvasDom.width / that.positionInfo[index].sourceW);
             realY = that.positionInfo[index].h * (that.canvasDom.height / that.positionInfo[index].sourceH);
 
-            beginX = that.positionInfo[index].offX * (that.canvasDom.width / that.positionInfo[index].sourceW );
-            beginY = that.positionInfo[index].offY * (that.canvasDom.height / that.positionInfo[index].sourceH );
+            beginX = that.positionInfo[index].offX * (that.canvasDom.width / that.positionInfo[index].sourceW);
+            beginY = that.positionInfo[index].offY * (that.canvasDom.height / that.positionInfo[index].sourceH);
 
-            ctx.clearRect(0,0,that.canvasDom.width,that.canvasDom.height);
+            ctx.clearRect(0, 0, that.canvasDom.width, that.canvasDom.height);
             ctx.drawImage(img, that.positionInfo[index].x, that.positionInfo[index].y,
-                that.positionInfo[index].w , that.positionInfo[index].h, beginX, beginY, realX, realY);
-
+                that.positionInfo[index].w, that.positionInfo[index].h, beginX, beginY, realX, realY);
         };
         img.src = this.imgSrc;
     }
 
-    begin(){
-
+    begin(beginIndex = 0) {
         let that = this;
 
-        if(this.ifbegin) return;
+        this.frameIndex = beginIndex;
+
+        if (this.ifbegin) {
+            return;
+        }
 
         this.ifbegin = true;
 
         let ctx = this.canvasDom.getContext('2d');
 
         let img = new Image();
-        img.onload = function(){
-            that.canvasDom.style.backgroundImage = "";
+        img.onload = function() {
+            that.canvasDom.style.backgroundImage = 'none';
 
             function nextFrame() {
-
                 that.frameIndex++;
-                if(that.frameIndex<that.positionInfo.length){
-
-                } else{
-                    that.frameIndex=0;
+                if (that.frameIndex >= that.positionInfo.length) {
+                    that.frameIndex = 0;
                 }
 
-                let beginX,beginY,realX,realY;
+                let beginX;
+                let beginY;
+                let realX;
+                let realY;
 
                 realX = that.positionInfo[that.frameIndex].w * (that.canvasDom.width / that.positionInfo[that.frameIndex].sourceW);
                 realY = that.positionInfo[that.frameIndex].h * (that.canvasDom.height / that.positionInfo[that.frameIndex].sourceH);
 
-                beginX = that.positionInfo[that.frameIndex].offX * (that.canvasDom.width / that.positionInfo[that.frameIndex].sourceW );
-                beginY = that.positionInfo[that.frameIndex].offY * (that.canvasDom.height / that.positionInfo[that.frameIndex].sourceH );
+                beginX = that.positionInfo[that.frameIndex].offX * (that.canvasDom.width / that.positionInfo[that.frameIndex].sourceW);
+                beginY = that.positionInfo[that.frameIndex].offY * (that.canvasDom.height / that.positionInfo[that.frameIndex].sourceH);
 
-                ctx.clearRect(0,0,that.canvasDom.width,that.canvasDom.height);
+                ctx.clearRect(0, 0, that.canvasDom.width, that.canvasDom.height);
                 ctx.drawImage(img, that.positionInfo[that.frameIndex].x, that.positionInfo[that.frameIndex].y,
-                    that.positionInfo[that.frameIndex].w , that.positionInfo[that.frameIndex].h, beginX, beginY, realX, realY);
+                    that.positionInfo[that.frameIndex].w, that.positionInfo[that.frameIndex].h, beginX, beginY, realX, realY);
 
-                that.timeOutIndex = setTimeout(nextFrame,that.interFrameTime);
+                that.timeOutIndex = setTimeout(nextFrame, that.interFrameTime);
             }
-
             nextFrame();
-
         };
         img.src = this.imgSrc;
-
     }
 
-    stop(){
+    stop() {
         this.ifstop = true;
         clearTimeout(this.timeOutIndex);
     }
 
-    recover(){
-        if(this.ifstop && this.ifbegin) {
+    recover() {
+        if (this.ifstop && this.ifbegin) {
             this.ifstop = false;
             this.begin();
         }
     }
 
-    end(){
+    end() {
         this.ifstop = false;
         this.ifbegin = false;
         this.frameIndex = 0;
